173b27278ef2cadc4f7d661431bbeb8cc18ed8ee
nagiyu <otonagiyu@gmail.com>
2025-10-04 00:40:12 +0900

    feat: Enhance DirectionStack component with additional justifyContent options and style prop; add SportsEsports icon component

--- common/components/Layout/Stacks/DirectionStack.tsx
+++ common/components/Layout/Stacks/DirectionStack.tsx
@@ -4,8 +4,9 @@ import { Stack } from '@mui/material';
 
 type DirectionStackProps = {
     spacing?: number;
-    justifyContent?: 'flex-start' | 'center' | 'flex-end';
+    justifyContent?: 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly';
     alignItems?: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline';
+    style?: React.CSSProperties;
     children: React.ReactNode;
 };
 
@@ -13,6 +14,7 @@ export default function DirectionStack({
     spacing = 2,
     justifyContent = 'center',
     alignItems = 'center',
+    style,
     children,
 }: DirectionStackProps) {
     return (
@@ -21,6 +23,7 @@ export default function DirectionStack({
             spacing={spacing}
             justifyContent={justifyContent}
             alignItems={alignItems}
+            style={style}
         >
             {children}
         </Stack>
